0a1c491be29c60e773eb275e3029dd115df8d36d
imprfct <kurchevskiy.personal@ya.ru>
2021-02-11 19:27:23 +0300

    feat: Added basic attack system

--- Assets/EnemiesManager.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemiesManager : MonoBehaviour
-{
-    public List<GameObject> enemies;
-}
--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -4,11 +4,19 @@ public class PlayerAnimationController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private PlayerMovementController movementController;
+    [SerializeField] private PlayerShootingController shootingController;
     
     private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    private static readonly int Attack = Animator.StringToHash("isAttackingWithFirstAnimation");
 
     private void Update()
     {
-        animator.SetBool(IsRunning, movementController.IsPlayerRunning());
+        if(movementController.IsPlayerRunning())
+            animator.SetBool(IsRunning, true);
+        else
+        {
+            animator.SetBool(IsRunning, false);
+            animator.SetBool(Attack, true);
+        }
     }
 }
